fa800f1cbaffe499e19f1f90a747ac689f27644d
pengjiazhen <jzpenga@gmail.com>
2019-02-26 18:05:44 +0800

    添加订阅回调类实体

---
+++ src/main/java/com/msp/impulse/nb/entity/SubscribeInfoEntity.java
@@ -0,0 +1,23 @@
+package com.msp.impulse.nb.entity;
+
+public class SubscribeInfoEntity {
+
+    private String callbackUrl;
+    private String userName;
+
+    public String getCallbackUrl() {
+        return callbackUrl;
+    }
+
+    public void setCallbackUrl(String callbackUrl) {
+        this.callbackUrl = callbackUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
