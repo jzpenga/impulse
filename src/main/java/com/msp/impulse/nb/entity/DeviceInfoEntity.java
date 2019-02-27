d43f4af33264a0d61f2e31e0945b4dbb5e3102dc
pengjiazhen <jzpenga@gmail.com>
2019-02-27 19:45:54 +0800

    添加NB管理类

---
+++ src/main/java/com/msp/impulse/nb/entity/DeviceInfoEntity.java
@@ -0,0 +1,50 @@
+package com.msp.impulse.nb.entity;
+
+public class DeviceInfoEntity {
+    private String deviceId;
+    private String name;
+    private String deviceType;
+    private String deviceModel;
+    private String nodeId;
+
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public void setDeviceModel(String deviceModel) {
+        this.deviceModel = deviceModel;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+}
