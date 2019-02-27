8ce9fbb885d157913003678e005053bc86fe4983
fineyjt <13261900961@163.com>
2019-02-27 17:38:17 +0800

    DATAREPORT update

--- src/main/java/com/msp/impulse/dao/DataReportDao.java
+++ src/main/java/com/msp/impulse/dao/DataReportDao.java
@@ -2,6 +2,10 @@ package com.msp.impulse.dao;
 
 import com.msp.impulse.nb.entity.DataReportEntity;
 
+import java.util.List;
+
 public interface DataReportDao {
     void save(DataReportEntity dataReportEntityList);
+
+    List<DataReportEntity> findByDataMark(String dataMark);
 }
--- src/main/java/com/msp/impulse/dao/impl/DataReportDaoImpl.java
+++ src/main/java/com/msp/impulse/dao/impl/DataReportDaoImpl.java
@@ -4,7 +4,11 @@ import com.msp.impulse.dao.DataReportDao;
 import com.msp.impulse.nb.entity.DataReportEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class DataReportDaoImpl implements DataReportDao {
@@ -13,5 +17,13 @@ public class DataReportDaoImpl implements DataReportDao {
     @Override
     public void save(DataReportEntity dataReportEntity) {
             mongoTemplate.save(dataReportEntity);
+    }
+
+    @Override
+    public List<DataReportEntity> findByDataMark(String dataMark) {
+        Query query=new Query();
+        query.addCriteria(Criteria.where("dataMark").is(dataMark));
+        List<DataReportEntity> dataReportEntities = mongoTemplate.find(query, DataReportEntity.class);
+        return dataReportEntities;
     }
 }
