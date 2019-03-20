a71c432ac31af1c397873640dab68717b0d207d0
fineyjt <13261900961@163.com>
2019-03-20 17:09:49 +0800

    commit dataReport

---
+++ src/main/java/com/msp/impulse/vo/DataReportVo.java
@@ -0,0 +1,94 @@
+package com.msp.impulse.vo;
+
+public class DataReportVo {
+    private  String  sensorName;
+    private String  sensorModel;
+    private String sensorNo;
+    private String sensorType;
+    private String  deviceId;
+    private String userId;
+    private String companyName;
+    private  String gatewayName;
+    private  String serviceCode;
+    private  String codeName;
+
+    public String getSensorName() {
+        return sensorName;
+    }
+
+    public void setSensorName(String sensorName) {
+        this.sensorName = sensorName;
+    }
+
+    public String getSensorModel() {
+        return sensorModel;
+    }
+
+    public void setSensorModel(String sensorModel) {
+        this.sensorModel = sensorModel;
+    }
+
+    public String getSensorNo() {
+        return sensorNo;
+    }
+
+    public void setSensorNo(String sensorNo) {
+        this.sensorNo = sensorNo;
+    }
+
+    public String getSensorType() {
+        return sensorType;
+    }
+
+    public void setSensorType(String sensorType) {
+        this.sensorType = sensorType;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getGatewayName() {
+        return gatewayName;
+    }
+
+    public void setGatewayName(String gatewayName) {
+        this.gatewayName = gatewayName;
+    }
+
+    public String getServiceCode() {
+        return serviceCode;
+    }
+
+    public void setServiceCode(String serviceCode) {
+        this.serviceCode = serviceCode;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+}
