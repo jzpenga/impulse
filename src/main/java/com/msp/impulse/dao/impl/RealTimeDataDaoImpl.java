ac22406aacfd05776344444837017f1059f20062
fineyjt <13261900961@163.com>
2019-03-31 13:25:12 +0800

    1

--- src/main/java/com/msp/impulse/dao/impl/RealTimeDataDaoImpl.java
+++ src/main/java/com/msp/impulse/dao/impl/RealTimeDataDaoImpl.java
@@ -14,6 +14,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -30,9 +31,9 @@ public class RealTimeDataDaoImpl implements RealTimeDataDao {
 
     @Override
     public RealTimeData selectByDeviceIdAndDataKey(String deviceId, String dataKey) {
-        Query query=new Query();
+        Query query = new Query();
         query.addCriteria(Criteria.where("deviceId").is(deviceId).and("dataKey").is(dataKey));
-         RealTimeData realTimeData = mongoTemplate.findOne(query, RealTimeData.class);
+        RealTimeData realTimeData = mongoTemplate.findOne(query, RealTimeData.class);
         return realTimeData;
     }
 
@@ -44,37 +45,40 @@ public class RealTimeDataDaoImpl implements RealTimeDataDao {
     @Override
     public PageBean selectRealTimeDataInfo(DataHistoryQuery dataHistoryQuery) {
         //已被删除的设备不被查出
-        SensorExample sensorExample=new SensorExample();
+        SensorExample sensorExample = new SensorExample();
         sensorExample.createCriteria().andFlagEqualTo("1");
         List<Sensor> sensorList = sensorMapper.selectByExample(sensorExample);
-        List<String> deviceList=new ArrayList();
-        for (Sensor sensor:sensorList) {
+        List<String> deviceList = new ArrayList();
+        for (Sensor sensor : sensorList) {
             String deviceId = sensor.getDeviceId();
             deviceList.add(deviceId);
         }
-        Query query =new Query();
-        Criteria criteria=new Criteria();
+        Query query = new Query();
+        Criteria criteria = new Criteria();
         criteria.and("dataValue").exists(true);
         criteria.and("deviceId").nin(deviceList);
-        if(StringUtils.isNotBlank(dataHistoryQuery.getSensorName())){
+        if (StringUtils.isNotBlank(dataHistoryQuery.getSensorName())) {
             Pattern pattern = Pattern.compile("^.*" + dataHistoryQuery.getSensorName() + ".*$", Pattern.CASE_INSENSITIVE);
             criteria.and("sensorName").regex(pattern);
         }
-        if(StringUtils.isNotBlank(dataHistoryQuery.getGatewayName())) {
+        if (StringUtils.isNotBlank(dataHistoryQuery.getGatewayName())) {
             Pattern pattern = Pattern.compile("^.*" + dataHistoryQuery.getGatewayName() + ".*$", Pattern.CASE_INSENSITIVE);
             criteria.and("gatewayName").regex(pattern);
         }
 
-        if(StringUtils.isNotBlank(dataHistoryQuery.getSensorType())) {
-            Pattern pattern = Pattern.compile("^.*" + dataHistoryQuery.getSensorType() + ".*$", Pattern.CASE_INSENSITIVE);
+        if (StringUtils.isNotBlank(dataHistoryQuery.getSensorType())) {
+            String replace = dataHistoryQuery.getSensorType();
+            replace = replace.replace("(", "\\(");
+            replace = replace.replace(")", "\\)");
+            Pattern pattern = Pattern.compile("^.*" + replace + ".*$", Pattern.CASE_INSENSITIVE);
             criteria.and("dataKeyName").is(pattern);
 
         }
         Criteria eventTime = criteria.and("eventTime").nin("0");
-        if(StringUtils.isNotBlank(dataHistoryQuery.getReportDateFrom())){//上报时间 From
+        if (StringUtils.isNotBlank(dataHistoryQuery.getReportDateFrom())) {//上报时间 From
             eventTime.gte(dataHistoryQuery.getReportDateFrom());
         }
-        if(StringUtils.isNotBlank(dataHistoryQuery.getReportDateTo())){//上报时间to
+        if (StringUtils.isNotBlank(dataHistoryQuery.getReportDateTo())) {//上报时间to
             eventTime.lte(dataHistoryQuery.getReportDateTo());
         }
         Query query1 = query.addCriteria(criteria);
@@ -82,13 +86,13 @@ public class RealTimeDataDaoImpl implements RealTimeDataDao {
         //查询总条数
         Long totalRecord = mongoTemplate.count(query1, RealTimeData.class);
         Sort sort = new Sort(Sort.Direction.DESC, "createTime");
-        if(dataHistoryQuery.getPageNo()==null){
+        if (dataHistoryQuery.getPageNo() == null) {
             dataHistoryQuery.setPageNo(1);
         }
-        if(dataHistoryQuery.getPageSize()==null){
+        if (dataHistoryQuery.getPageSize() == null) {
             dataHistoryQuery.setPageSize(10);
         }
-        Pageable pageable = new PageRequest(dataHistoryQuery.getPageNo()-1, dataHistoryQuery.getPageSize(), sort);
+        Pageable pageable = new PageRequest(dataHistoryQuery.getPageNo() - 1, dataHistoryQuery.getPageSize(), sort);
         List<RealTimeData> realTimeDataList = mongoTemplate.find(query1.with(pageable), RealTimeData.class);
 
         PageBean pageBean = new PageBean(dataHistoryQuery.getPageNo(), dataHistoryQuery.getPageSize(), totalRecord.intValue());
